3ab1b5c0debe2543dc348dd1d04c7c250e9476c9
Neil <neilspurgeon@gmail.com>
2016-03-25 14:29:57 -0700

    init

---
+++ js/work-grid.js
@@ -0,0 +1,31 @@
+// external js: packery.pkgd.js
+$(document).ready(function() {
+
+  var preloadedContent = $('#preloaded-content');
+  var imgs = $('img');
+  var imgCount = imgs.length;
+
+  if (imgs[0]) {
+    // hide loading content
+    preloadedContent.css('opacity', '0');
+    console.log('loading...');
+    $('body').append('<img id="loading-icon" style="position: fixed; top: 40%; z-index: 99999; left: 50%; width: 60px; margin-left: -30px;" src="/user/themes/lwd/images/loading-icon.svg"/>');
+
+    // load each image
+    imgs.bind( 'load', function(){
+      imgCount --;
+
+      // On last img show content
+      if (imgCount === 0) {
+        $('#loading-icon').hide();
+        preloadedContent.css({'opacity': '1', 'transition': '.5s ease'});
+      }
+    });
+  }
+    $('.work-grid').packery({
+      itemSelector: '.grid-item',
+      gutter: 20,
+      transitionDuration: '0'
+    });
+});
+
