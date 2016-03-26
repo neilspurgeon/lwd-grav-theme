d172450621585bd9316df77c35be103859d28d7c
Neil <neilspurgeon@gmail.com>
2016-03-25 23:36:47 -0700

    removed loading

--- js/work-grid.js
+++ js/work-grid.js
@@ -5,23 +5,23 @@ $(document).ready(function() {
   var imgs = $('img');
   var imgCount = imgs.length;
 
-  if (imgs[0]) {
-    // hide loading content
-    preloadedContent.css('opacity', '0');
-    console.log('loading...');
-    $('body').append('<img id="loading-icon" style="position: fixed; top: 40%; z-index: 99999; left: 50%; width: 60px; margin-left: -30px;" src="/user/themes/lwd/images/loading-icon.svg"/>');
+  // if (imgs[0]) {
+  //   // hide loading content
+  //   preloadedContent.css('opacity', '0');
+  //   console.log('loading...');
+  //   $('body').append('<img id="loading-icon" style="position: fixed; top: 40%; z-index: 99999; left: 50%; width: 60px; margin-left: -30px;" src="/user/themes/lwd/images/loading-icon.svg"/>');
 
-    // load each image
-    imgs.bind( 'load', function(){
-      imgCount --;
+  //   // load each image
+  //   imgs.bind( 'load', function(){
+  //     imgCount --;
 
-      // On last img show content
-      if (imgCount === 0) {
-        $('#loading-icon').hide();
-        preloadedContent.css({'opacity': '1', 'transition': '.5s ease'});
-      }
-    });
-  }
+  //     // On last img show content
+  //     if (imgCount === 0) {
+  //       $('#loading-icon').hide();
+  //       preloadedContent.css({'opacity': '1', 'transition': '.5s ease'});
+  //     }
+  //   });
+  // }
     $('.work-grid').packery({
       itemSelector: '.grid-item',
       gutter: 20,
