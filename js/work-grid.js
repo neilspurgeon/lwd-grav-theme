511766c6093172acd3c509d38a415db5026a4647
Neil <neilspurgeon@gmail.com>
2016-12-25 17:51:07 -0800

    removed unused code for preloading content with loading icon

--- js/work-grid.js
+++ js/work-grid.js
@@ -1,24 +1,20 @@
 // external js: packery.pkgd.js
 $(document).ready(function() {
 
-  var preloadedContent = $('#preloaded-content');
   var imgs = $('img');
   var imgCount = imgs.length;
 
   if (imgs[0]) {
     // hide loading content
-    preloadedContent.css('visibility', 'hidden');
-    $('body').append('<img id="loading-icon" style="position: fixed; top: 40%; z-index: 99999; left: 50%; width: 60px; margin-left: -30px;" src="/user/themes/lwd/images/loading-icon.svg"/>');
 
     // load each image
     imgs.bind( 'load', function(){
       imgCount --;
+      // do something after each image is loaded
 
       // On last img show content
       if (imgCount === 0) {
-        $('#loading-icon').hide();
-        preloadedContent.css({'visibility': 'initial', 'opacity': '0', 'transition': '.5s ease'});
-        preloadedContent.css({'opacity': '1'});
+        // do something once all images have loaded
       }
     });
   }
